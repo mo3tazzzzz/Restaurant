cc0e69912e3149f89636921d95ff305c4481ab2c
mo3tazzzzz <m8264031@gmail.com>
2025-07-30 18:10:02 +0300

    restaurant project

---
+++ JS/main.js
@@ -0,0 +1,58 @@
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     margin:10,
+//     nav:false,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:5
+//         }
+//     }
+// })
+
+
+
+    // // Testimonials carousel
+    // $(".testimonial-carousel").owlCarousel({
+    //     autoplay: true,
+    //     smartSpeed: 1000,
+    //     center: true,
+    //     margin: 24,
+    //     dots: true,
+    //     loop: true,
+    //     nav : false,
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         },
+    //         992:{
+    //             items:3
+    //         }
+    //     }
+    // });
+    
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            
+            1000:{
+                items:3
+            }
+        }
+    })
+
